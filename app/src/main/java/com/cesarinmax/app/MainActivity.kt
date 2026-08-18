0af6be589936b7f120a0e88367b25c8beb7711e5
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-08-18 14:48:28 -0500

    Add import for Android Manifest

--- app/src/main/java/com/cesarinmax/app/MainActivity.kt
+++ app/src/main/java/com/cesarinmax/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cesarinmax.app
 
 import android.app.AlertDialog
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
