cd7f6f64f1abb1352cca74cea1aa607ad8abbb5a
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-08-20 00:47:51 -0500

    Delete .github/workflows director

--- app/src/main/java/com/cesarinmax/app/MainActivity.kt
+++ app/src/main/java/com/cesarinmax/app/MainActivity.kt
@@ -86,7 +86,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun cargarConfigGist() {
         try {
-            val urlGist = "https://gist.githubusercontent.com/kilomkolim84-rgb/06685708f1b31fa79cd898b90333e315/raw/a9d9d0b01d4b98944aeb18a53b25f55cb9fa816e/cesarin_max_config.json?t=" + System.currentTimeMillis()
+            // ✅ HASH NUEVO DEL GIST — LEE LA DIRECCIÓN CORRECTA
+            val urlGist = "https://gist.githubusercontent.com/kilomkolim84-rgb/06685708f1b31fa79cd898b90333e315/raw/9548b01255505e67f02f7add5aeedbc8ff2bb510/cesarin_max_config.json?t=" + System.currentTimeMillis()
             configGist = JSONObject(URL(urlGist).readText())
             val cr = configGist?.getJSONObject("config_red")
             ssidEsperado = cr?.optString("ssid_esperado", "CESARINMAX")!!
